f265baf46ba0815814868059feac59c4284d1517
Bgdan1802 <bgdan1802@gmail.com>
2015-11-24 02:45:32 +0200

    VK auth

--- app/src/main/java/com/kievon/MainActivity.java
+++ app/src/main/java/com/kievon/MainActivity.java
@@ -1,27 +1,40 @@
 package com.kievon;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.places.Places;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKScope;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
+import com.vk.sdk.util.VKUtil;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 
 public class MainActivity extends FragmentActivity
         implements ConnectionCallbacks, OnConnectionFailedListener {
 
     private GoogleApiClient mGoogleApiClient;
-
+    private String[] scope = new String[] {VKScope.MESSAGES, VKScope.FRIENDS, VKScope.WALL };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        VKSdk.login(this, scope);
         mGoogleApiClient = new GoogleApiClient
                 .Builder(this)
                 .addApi(Places.GEO_DATA_API)
@@ -31,6 +44,23 @@ public class MainActivity extends FragmentActivity
                 .build();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+// Пользователь успешно авторизовался
+                Toast.makeText(getApplicationContext(),"GOOD", Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onError(VKError error) {
+// Произошла ошибка авторизации (например, пользователь запретил авторизацию)
+                Toast.makeText(getApplicationContext(),"Error", Toast.LENGTH_LONG).show();
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
     @Override
     protected void onStart() {
         super.onStart();
@@ -79,4 +109,5 @@ public class MainActivity extends FragmentActivity
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
+
 }
