95cca856c8fd32a8e0844e14a8d5b7f6e5951142
Trianarius <trianarius@gmail.com>
2015-12-20 14:35:21 +0200

    upd
    upd

--- app/src/main/java/com/kievon/MapActivity.java
+++ app/src/main/java/com/kievon/MapActivity.java
@@ -2,12 +2,17 @@ package com.kievon;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.PlaceLikelihood;
 import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
 import com.google.android.gms.location.places.Places;
 
@@ -16,7 +21,6 @@ import com.google.android.gms.location.places.Places;
  */
 public class MapActivity extends FragmentActivity implements ConnectionCallbacks, OnConnectionFailedListener {
 
-    public static final String TAG = "MainActivity";
     private GoogleApiClient mapGoogleApiClient;
 
     @Override
@@ -33,7 +37,34 @@ public class MapActivity extends FragmentActivity implements ConnectionCallbacks
                 .build();
     }
 
-    @Override
+    public void onPlaceLikelihood(View view) {
+        Log.i("Place", "onPlaceLikelihood");
+        if (mapGoogleApiClient != null) {
+
+            PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
+                    .getCurrentPlace(mapGoogleApiClient, null);
+
+            result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+                @Override
+                public void onResult(PlaceLikelihoodBuffer likelyPlaces) {
+                    Log.i("Place", "onResult");
+                    if (likelyPlaces.getCount() <= 0) {
+                        Toast.makeText(MapActivity.this, "No place found", Toast.LENGTH_SHORT).show();
+                    }
+                    for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+                        Log.i("Place", String.format("Place '%s' has likelihood: %g",
+                                placeLikelihood.getPlace().getName(),
+                                placeLikelihood.getLikelihood()));
+                    }
+                    likelyPlaces.release();
+                }
+            });
+        } else {
+            Toast.makeText(MapActivity.this, "No GoogleApiClient", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+        @Override
     protected void onStart() {
         super.onStart();
         mapGoogleApiClient.connect();
@@ -47,21 +78,19 @@ public class MapActivity extends FragmentActivity implements ConnectionCallbacks
 
     @Override
     public void onConnected(Bundle bundle) {
-
+        Toast.makeText(this, "onConnected", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        Toast.makeText(this, "onConnectionSuspended", Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-
+        Toast.makeText(this, "onConnectionFailed", Toast.LENGTH_LONG).show();
     }
 
-    PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
-            .getCurrentPlace(mapGoogleApiClient, null);
 
 
 
