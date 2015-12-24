a5392800a0b387178fed16a12791e6174a5906d4
Bgdan1802 <bgdan1802@gmail.com>
2015-12-24 02:35:40 +0200

    new version 1

--- app/src/main/java/com/kievon/MainActivity.java
+++ app/src/main/java/com/kievon/MainActivity.java
@@ -1,47 +1,57 @@
 package com.kievon;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
+
+
+
 import com.google.android.gms.location.places.Places;
 
 
 public class MainActivity extends FragmentActivity
-        implements ConnectionCallbacks, OnConnectionFailedListener {
+        implements ConnectionCallbacks, OnConnectionFailedListener, View.OnClickListener {
 
     private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.auth);
 
-        mGoogleApiClient = new GoogleApiClient
-                .Builder(this)
-                .addApi(Places.GEO_DATA_API)
-                .addApi(Places.PLACE_DETECTION_API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
+        findViewById(R.id.back).setOnClickListener(this);
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+//        BuildConfig.VERSION_NAME;
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
                 .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
-    }
+//    public void ClickBack(){
+//        Intent intent1 = new Intent(Autorization.this, MainActivity.class);
+//        startActivity(intent1);
 
-    @Override
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
-    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -78,5 +88,12 @@ public class MainActivity extends FragmentActivity
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("123", "Text");
+        Intent signinIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signinIntent, 1);
     }
 }
