4f40e9e828ece9cc73d4e88e37ea79238176dd2f
Bgdan1802 <bgdan1802@gmail.com>
2015-12-30 09:36:44 +0200

    version 1.1

--- app/src/main/java/com/kievon/MainActivity.java
+++ app/src/main/java/com/kievon/MainActivity.java
@@ -1,15 +1,22 @@
 package com.kievon;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
@@ -17,83 +24,120 @@ import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListe
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 
-
-
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
 
-public class MainActivity extends FragmentActivity
-        implements ConnectionCallbacks, OnConnectionFailedListener, View.OnClickListener {
-
-    private GoogleApiClient mGoogleApiClient;
+  SupportMapFragment mapFregment;
+  GoogleMap map;
+  final String TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.auth);
 
-        findViewById(R.id.back).setOnClickListener(this);
-        findViewById(R.id.sign_in_button).setOnClickListener(this);
-
-//        BuildConfig.VERSION_NAME;
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-
-    }
-
-//    public void ClickBack(){
-//        Intent intent1 = new Intent(Autorization.this, MainActivity.class);
-//        startActivity(intent1);
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btnTest3).setOnClickListener(this);
+        findViewById(R.id.btnTest).setOnClickListener(this);
+        findViewById(R.id.btnTest2).setOnClickListener(this);
+        mapFregment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        map=mapFregment.getMap();
+        if(map==null){
+            finish();
+            return;
         }
-
-        return super.onOptionsItemSelected(item);
+        init();
     }
 
-    @Override
-    public void onConnected(Bundle bundle) {
+    private void init() {
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                Log.d(TAG,"onClickMap:"+latLng.latitude+","+ latLng.longitude);
+            }
+        });map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                Log.d(TAG, "onLongClickMap:" + latLng.latitude + "," + latLng.longitude);
+            }
+        });
+        map.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                Log.d(TAG,"onCameraChange:"+ cameraPosition.target.latitude+ " " +cameraPosition.target.longitude);
+            }
+        });
 
     }
 
-    @Override
-    public void onConnectionSuspended(int i) {
+    private void onClickTest2(View view){
+        map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+    }
+    private void onClickTest(View view){
+
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+
 
     }
 
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
 
-    }
+
 
     @Override
     public void onClick(View v) {
-        Log.d("123", "Text");
-        Intent signinIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signinIntent, 1);
+        switch (v.getId()){
+            case R.id.btnTest2:
+                onClickTest(v);
+                break;
+            case R.id.btnTest:
+                onClickTest2(v);
+                break;
+            case R.id.btnTest3:
+                Intent intent = new Intent(MainActivity.this, Authorization.class);
+                startActivity(intent);
+                break;
+
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
